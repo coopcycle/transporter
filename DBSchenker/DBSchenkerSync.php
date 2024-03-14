bd9b01c4e662fbe6c3610c5b1f228070334591a3
Antoine Bagnaud <contact@antoinebagnaud.me>
2024-03-14 14:53:49 +0100

    fix(sync): fix wrong condition in flush

--- DBSchenker/DBSchenkerSync.php
+++ DBSchenker/DBSchenkerSync.php
@@ -34,7 +34,7 @@ class DBSchenkerSync {
      */
     public function flush(bool $dry_run = false): void
     {
-        if ($dry_run) {
+        if (!$dry_run) {
             array_walk($this->unflushed, fn ($file) => $this->options->getFilesystem()->delete($file['path']));
         }
         $this->unflushed = [];
