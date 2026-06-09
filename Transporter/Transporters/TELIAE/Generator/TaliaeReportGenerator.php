92c4b181e07f9b6872d1cccd91b9fccbc9522551
Antoine Bagnaud <contact@antoinebagnaud.me>
2026-06-09 09:50:15 +0200

    feat(teliae): swap NAD segments.

--- Transporter/Transporters/TELIAE/Generator/TaliaeReportGenerator.php
+++ Transporter/Transporters/TELIAE/Generator/TaliaeReportGenerator.php
@@ -2,7 +2,29 @@
 
 namespace Transporter\Transporters\TELIAE\Generator;
 
+use EDI\Generator\Segment\NameAndAddress;
 use Transporter\Generator\EDIFACTReportGenerator;
 
 class TaliaeReportGenerator extends EDIFACTReportGenerator
-{ }
+{
+
+    #[\Override]
+    protected function getAgencyNAD(): NameAndAddress
+    {
+        return (new NameAndAddress())
+            ->setPartyFunctionCodeQualifier('MR')
+            ->setPartyIdentificationDetails($this->options->getAgencySiret(), '5')
+            ->setPartyName([$this->options->getAgencyName()]);
+    }
+
+
+    #[\Override]
+    protected function getCoopNAD(): NameAndAddress
+    {
+        return (new NameAndAddress())
+            ->setPartyFunctionCodeQualifier('MS')
+            ->setPartyIdentificationDetails($this->options->getCoopSiret(), '5')
+            ->setPartyName([$this->options->getCoopName()]);
+    }
+
+}
